950447c56d71d646128164dccddd4dbb3d93ecc2
IrelDev <ireldeveloper@gmail.com>
2020-09-14 13:59:58 +0500

    Add macOS Views

---
+++ macOS/MacOSMainView.swift
@@ -0,0 +1,27 @@
+//
+//  MacOSMainView.swift
+//  Tests (macOS)
+//
+//  Created by Kirill Pustovalov on 12.09.2020.
+//
+
+import SwiftUI
+
+struct MacOSMainView: View {
+    var body: some View {
+        NavigationView {
+            Group {
+                FolderListView()
+                MacOSTestListView()
+                TestView()
+            }
+        }
+        .navigationTitle("")
+    }
+}
+
+struct MacOSMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MacOSMainView()
+    }
+}
