a23dfd765e15316cf88c32afd8fbfb8de72a3d97
IrelDev <ireldeveloper@gmail.com>
2020-09-14 13:50:37 +0500

    Initial Commit

---
+++ Shared/TestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestsApp.swift
+//  Shared
+//
+//  Created by Kirill Pustovalov on 14.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct TestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
