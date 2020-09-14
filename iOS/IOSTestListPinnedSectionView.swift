b45d52396a0eb513ec4e547320d74b62a731b340
IrelDev <ireldeveloper@gmail.com>
2020-09-14 13:59:31 +0500

    Add iOS Views

---
+++ iOS/IOSTestListPinnedSectionView.swift
@@ -0,0 +1,29 @@
+//
+//  IOSTestListPinnedSectionView.swift
+//  Tests (iOS)
+//
+//  Created by Kirill Pustovalov on 13.09.2020.
+//
+
+import SwiftUI
+
+struct IOSTestListPinnedSectionView: View {
+    @Binding var isPinnedSectionExpanded: Bool
+    var body: some View {
+        HStack {
+            TestListPinnedSectionView()
+            Spacer()
+            
+            Image(systemName: isPinnedSectionExpanded ? "chevron.down" : "chevron.right").adoptToImageModifier()
+                .onTapGesture {
+                    isPinnedSectionExpanded.toggle()
+                }
+        }
+    }
+}
+
+struct IOSTestListPinnedSection_Previews: PreviewProvider {
+    static var previews: some View {
+        IOSTestListPinnedSectionView(isPinnedSectionExpanded: .constant(true))
+    }
+}
