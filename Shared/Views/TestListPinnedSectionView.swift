1d220512d86ab7db8c47145bb6a1582fa5c63699
IrelDev <ireldeveloper@gmail.com>
2020-09-14 14:01:03 +0500

    Add shared views

---
+++ Shared/Views/TestListPinnedSectionView.swift
@@ -0,0 +1,32 @@
+//
+//  TestListPinnedSectionView.swift
+//  Tests
+//
+//  Created by Kirill Pustovalov on 11.09.2020.
+//
+
+import SwiftUI
+
+struct TestListPinnedSectionView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 2) {
+            HStack {
+                Image(systemName: "pin.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 12)
+                    .foregroundColor(Color(.systemGray))
+                Text("Pinned")
+                    .fontWeight(.semibold)
+                    .font(.callout)
+                    .foregroundColor(Color(.systemGray))
+            }
+        }
+    }
+}
+
+struct TestListPinnedSectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestListPinnedSectionView()
+    }
+}
