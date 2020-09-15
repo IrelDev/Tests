65964769b16a35982369f7b3d06dd5dc7ba7a5b2
IrelDev <ireldeveloper@gmail.com>
2020-09-15 10:01:25 +0500

    Update TestView to support paging

--- Shared/Views/TestView.swift
+++ Shared/Views/TestView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct TestView: View {
+    @State var selection = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Math test 10.06.20")
+                .font(.system(size: 40, weight: .bold))
+                .foregroundColor(.white)
+                .padding()
+            Spacer()
+            GeometryReader { reader in
+                HStack {
+                    Spacer()
+                    TabView(selection: $selection) {
+                        ForEach(0..<50) { index in
+                            ZStack {
+                                Color(.systemPurple)
+                                Text("\(index)")
+                            }
+                        }
+                    }
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+                    .cornerRadius(25)
+                    .frame(width: reader.size.width / 1.2, height: reader.size.height / 1.2)
+                    Spacer()
+                }
+            }
+            Spacer()
+        }
+        .navigationBarTitle("", displayMode: .inline)
     }
 }
-
 struct TestView_Previews: PreviewProvider {
     static var previews: some View {
         TestView()
