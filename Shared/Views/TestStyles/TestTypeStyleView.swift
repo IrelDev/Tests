a4b4f5602f6b87c3def4f7069c4c67c8940fffdf
IrelDev <ireldeveloper@gmail.com>
2020-09-30 10:14:35 +0500

    Redesign type test style

--- Shared/Views/TestStyles/TestTypeStyleView.swift
+++ Shared/Views/TestStyles/TestTypeStyleView.swift
@@ -9,35 +9,27 @@ import SwiftUI
 
 struct TestTypeStyleView: View {
     @State var isTextFieldExpanded: Bool = false
+    @State var answer: String = ""
+    @State var submitted = false
     
     var body: some View {
         GeometryReader { reader in
             VStack {
                 Spacer()
                 ScrollView(.vertical) {
-                    Text("Question 1/100: Write the function parameter that allows to modify a variable defined outside the function.")
+                    Text("Question 1/100: Write the function parameter that allows to modify a variable defined outside the function.Question 1/100: Write the function parameter that allows to modify a variable defined")
                         .font(.title2)
                 }
-                .padding()
-                Spacer()
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 25)
-                                .frame(width: reader.size.width / 4, height: reader.size.width / 4)
-                                .foregroundColor(Color(.systemGray5))
-                            Button(action: {
-                                
-                            }, label: {
-                                Image(systemName: "pencil").adoptToImageModifier(withSize: reader.size.width / 10)
-                            })
-                        }
-                        Spacer()
-                    }
-                    Spacer()
+                .padding([.leading, .trailing, .top])
+                .onTapGesture {
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
                 }
+                TextField("Write your answer here", text: $answer)
+                    .onTapGesture { }
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .cornerRadius(25)
+                    .frame(width: reader.size.width * 0.9)
+                    .padding()
             }
         }
     }
